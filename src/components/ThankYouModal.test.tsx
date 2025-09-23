61039148ad6b2e111943fe80c282350fe43bafe0
Ben Wencke <benwencke@gmail.com>
2025-09-22 21:20:18 -0400

    Remove check for options in thank you modal

--- src/components/ThankYouModal.test.tsx
+++ src/components/ThankYouModal.test.tsx
@@ -6,21 +6,5 @@ describe('ThankYouModal', () => {
   it('renders a thank you message and icon', async () => {
     render(<ThankYouModal onDismiss={() => {}} />);
     expect(await screen.findByText(/Thank You!/i)).toBeInTheDocument();
-    // At least one of the possible messages should be present
-    expect(
-      await screen.findByText(
-        (content) =>
-          content.includes('paws') ||
-          content.includes('fur') ||
-          content.includes('mutt') ||
-          content.includes('Howl') ||
-          content.includes('tails wag') ||
-          content.includes('purr') ||
-          content.includes('cat') ||
-          content.includes('claw') ||
-          content.includes('feline') ||
-          content.includes('lion')
-      )
-    ).toBeInTheDocument();
   });
 });
