65356ec2556d004eb4fa518010f7802495627e7b
Ben Wencke <benwencke@gmail.com>
2025-09-28 10:25:01 -0400

    Change package name to com.hancockpaws.app to since com.paws.app was already taken

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.paws.app',
+  appId: 'com.hancockpaws.app',
   appName: 'paws',
   webDir: 'dist',
   plugins: {
