5e3dbfacdef0294cc3c9004026941e45303f4c74
Ben Wencke <benwencke@gmail.com>
2026-04-21 22:21:09 -0400

    Updates

--- assets/md.transition-legacy-btPGydSn.js
+++ assets/md.transition-legacy-btPGydSn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DB0hLShC.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Nw96Q2wo.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
