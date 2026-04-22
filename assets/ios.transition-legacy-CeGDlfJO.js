1b7673e9ec59cbf2327f5899dec361c1282f6f5e
Ben Wencke <benwencke@gmail.com>
2026-04-21 23:18:47 -0400

    Updates

--- assets/ios.transition-legacy-CeGDlfJO.js
+++ assets/ios.transition-legacy-CeGDlfJO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgSCBern.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-F6wnVGHP.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
