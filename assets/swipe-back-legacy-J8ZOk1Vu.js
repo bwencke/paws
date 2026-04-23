c196934fc6f8209d9052630067a2f7b5c3cd4644
Ben Wencke <benwencke@gmail.com>
2026-04-22 21:30:14 -0400

    Updates

--- assets/swipe-back-legacy-J8ZOk1Vu.js
+++ assets/swipe-back-legacy-J8ZOk1Vu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dee-80x2.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CJh_5VJY.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
